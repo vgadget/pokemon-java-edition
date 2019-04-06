d0803c44200388895c2a95c1d19edfded35cec8f
vgadget <adrianvazquezbarrera@outlook.com>
2019-04-06 22:00:26 +0200

    Custom button added for menu.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        //Debug
+        //Debug - IN DEV
         debug.PokemonBattleGraphicControlerDebug.test();
     }
 
